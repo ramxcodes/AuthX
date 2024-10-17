b9cbafce786a3bb9faaf1fc2969390e5a66f75cb
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-17 08:14:58 +0530

    Initiaziled & Added Mongo DB 📅

---
+++ BackEnd/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from "dotenv"
+
+import { connectDB } from "./db/connectDB.js"
+import authRoutes from "./routes/auth.route.js"
+
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send("Hellow World")
+})
+
+app.use("/api/auth", authRoutes)
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("server is running");
+    
+})
