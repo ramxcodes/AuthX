62ba8f590aa7f49edea78a3ae0a27a195684aa26
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-18 15:45:37 +0530

    SignUp Endpoint 👨‍💻

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import dotenv from "dotenv"
-
 import { connectDB } from "./db/connectDB.js"
 import authRoutes from "./routes/auth.route.js"
-
 
 dotenv.config()
 
@@ -11,19 +9,8 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 
-app.get('/', (req, res) => {
-    res.send("Hellow World")
-})
-
+app.use(express.json());
 app.use("/api/auth", authRoutes)
-
-
-
-
-
-
-
-
 
 
 app.listen(PORT, () => {
