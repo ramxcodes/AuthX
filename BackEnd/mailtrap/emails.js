71db3c497b6fdd22c2ea09ae8a153c8fd66b0159
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-20 11:24:09 +0530

    Added Forgot Password Endpoint 🔄

--- BackEnd/mailtrap/emails.js
+++ BackEnd/mailtrap/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
+import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
 import { mailtrapClient, sender } from "./mailtrap.config.js"
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -37,5 +37,24 @@ export const sendWelcomeEmail = async (email, name) => {
     } catch (error) {
         console.error(`Error Sending Welcome Email`, error)
         throw new Error(`Error sending Welcome Email: ${error}`)
+    }
+}
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+    const recipient = [{email}]
+    try {
+       
+        const response = await mailtrapClient.send ({
+            from: sender,
+            to: recipient,
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+            category: "password Reset"
+        });
+        console.log("Welcome Email sent successfully", response);
+
+    } catch (error) {
+        console.error ("Error sending password reset email" ,error)
+        throw new Error (`Error sending password reset email: ${error}` )
     }
 }
