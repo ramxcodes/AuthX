d6cbe4576015ddf6fe55113ea88a6c44d7697c27
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-20 12:47:37 +0530

    Added Check Auth Endpoint ✅

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -202,3 +202,25 @@ export const resetPassword = async (req, res) => {
         });
     }
 };
+
+export const checkAuth = async (req, res) => {
+	try {
+		const user = await User.findById(req.userId).select("-password")
+		if(!user){
+			return res.status(400).json({
+				success : false,
+				message: "User not found"
+			})
+		}
+
+		res.status(200).json({ success: true, user })
+
+
+	} catch (error) {
+		Console.log("Error in checkAuth ", error)
+		res.status(400).json({
+			success: false,
+			message: error.message
+		})
+	}
+}
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv"
+import cookieParser from 'cookie-parser'
 import { connectDB } from "./db/connectDB.js"
 import authRoutes from "./routes/auth.route.js"
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes)
 
 
