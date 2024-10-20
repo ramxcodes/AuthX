08a4b27945469c64f939e08874884c32244a6559
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-20 10:58:15 +0530

    Added Login Endpoint 🔑

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -83,7 +83,40 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req,res) => {
-    res.send("login Route")
+	const {email, password} = req.body;
+    try {
+		const user = await User.findOne({ email });
+		if(!user){
+			return res.status(400).json({ 
+				success: false, 
+				message: "Invalid Credential"
+			})
+		}
+		const isPasswordValid = await bcryptjs.compare(password, user.password)
+		if(!isPasswordValid){
+			return res.status(400).json({ 
+				success: false, 
+				message: "Invalid Credential"
+			})
+		}
+		generateTokenAndSetCookie(res, user._id)
+
+		user.lastLogin = new Date();
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Login successfull",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+		
+	} catch (error) {
+		console.log("Error in Login ", error);
+		res.status(500).json({ success: false, message: error.message });
+	}
 }
 export const logout = async (req,res) => {
     res.clearCookie("token");
