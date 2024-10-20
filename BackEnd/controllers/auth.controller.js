1b4961a0d15496dd227ccf87cbdfbad37db23653
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-20 10:42:45 +0530

    Added Logout Endpoint 🚪

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -86,6 +86,9 @@ export const login = async (req,res) => {
     res.send("login Route")
 }
 export const logout = async (req,res) => {
-    res.send("logout Route")
+    res.clearCookie("token");
+	res.status(200).json({
+		success: true,
+		message:"Logged out successfully"
+	})
 }
-
