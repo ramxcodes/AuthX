46d8304727e6aab15a4453785c7c501fd7b37286
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-20 10:14:30 +0530

    Added Verify Email Endpoint 🔍

--- BackEnd/mailtrap/emails.js
+++ BackEnd/mailtrap/emails.js
@@ -18,3 +18,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending Verification Email: ${error}`)
     }
 }
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{email}]
+    try {
+       
+        const response = await mailtrapClient.send ({
+            from: sender,
+            to: recipient,
+            template_uuid: "be53c495-b94e-44d0-8778-a30ba20387f1",
+            template_variables: {
+                "company_info_name": "AuthX",
+                "name": name,
+              }
+        });
+        console.log("Welcome Email sent successfully", response);
+
+    } catch (error) {
+        console.error(`Error Sending Welcome Email`, error)
+        throw new Error(`Error sending Welcome Email: ${error}`)
+    }
+}
